e2cb33943e2c942e63e2d60104670cf1cf9e820c
kristina58 <166289675+kristina58@users.noreply.github.com>
2024-05-18 20:56:18 +0200

    Add files via upload

---
+++ 3.4.1.6 lab.py
@@ -0,0 +1,12 @@
+liczby_z_kapelusza = [1, 2, 3, 4, 5]  # Istniejąca lista liczb ukrytych w kapeluszu.
+
+liczby_z_kapelusza[2]= input ("Wprowadź liczbę calkowitą: ")# Krok 1: Napisz wiersz kodu, który prosi użytkownika
+print(liczby_z_kapelusza)# o zastąpienie środkowego numeru liczbą całkowitą wprowadzoną przez użytkownika.
+
+del liczby_z_kapelusza[-1]# Krok 2: Napisz tutaj wiersz kodu, który usuwa ostatni element z listy.
+
+print(liczby_z_kapelusza)
+
+print("Długość listy: ", len(liczby_z_kapelusza))# Krok 3: Napisz tutaj wiersz kodu, który wypisuje długość istniejącej listy.
+
+print(liczby_z_kapelusza)  # Wyświetlanie zawartości listy.
