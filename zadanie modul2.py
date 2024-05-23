df8559360aa9f89a409a42b1ca812f82ab17129c
kristina58 <166289675+kristina58@users.noreply.github.com>
2024-05-23 16:54:09 +0200

    Add files via upload

---
+++ zadanie modul2.py
@@ -0,0 +1,15 @@
+kilometry = int(input("Wpisz odległość w km: "))
+milimetry = kilometry * 100000
+centymetry = kilometry * 10000
+metry = kilometry * 1000
+mile = kilometry * 0.6214
+stope = kilometry * 3280.84
+cale = kilometry * 39370
+
+print("Odległość w kilometrach: ", kilometry)
+print("Odległość w milimetrach: ", milimetry)
+print("Odległość w centymetrach: ", centymetry)
+print("Odległość w metrach: ", metry)
+print("Odległość w milach: ", mile)
+print("Odległość w stopach: ", stope)
+print("Odległość w calach: ", cale)
